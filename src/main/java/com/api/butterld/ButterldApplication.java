622be40e928701f02b76e4e7c7a6b2d9c08f2aaf
nekuButter <gcanovasabraham@gmail.com>
2023-05-08 15:57:16 +0200

    Init Maven Project

---
+++ src/main/java/com/api/butterld/ButterldApplication.java
@@ -0,0 +1,13 @@
+package com.api.butterld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ButterldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ButterldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/butterld/ButterldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.butterld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ButterldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
